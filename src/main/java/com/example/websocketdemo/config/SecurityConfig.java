e164961be3ffd6e39d3289edfc93fd2de6dd45f1
TatucRobert <68648367+TatucRobert@users.noreply.github.com>
2022-09-24 01:52:10 +0300

    Add files via upload

---
+++ src/main/java/com/example/websocketdemo/config/SecurityConfig.java
@@ -0,0 +1,23 @@
+package com.example.websocketdemo.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@Configuration
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Autowired
+    public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+        PasswordEncoder encoder = PasswordEncoderFactories.createDelegatingPasswordEncoder();
+
+        auth.inMemoryAuthentication()
+                .withUser("admin")
+                .password(encoder.encode("passwd"))
+                .roles("USER");
+    }
+}
